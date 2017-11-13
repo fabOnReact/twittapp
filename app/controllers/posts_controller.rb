49a27f317b0a00f017ad89f1a4d0094d00e397f2
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-11-13 14:32:05 +0100

    add check @search.present?

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ require 'controller_modules'
 class PostsController < ApplicationController
   include Twitter
   def index
-    @search = client.search(params[:search], :lang => "en").take(10).collect if params[:search]
+    @search = client.search(params[:search], :lang => "en").take(10).collect if params[:search].present?
   end
 
   def new
